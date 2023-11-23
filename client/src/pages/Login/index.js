a054bc4a80b3393f98325e4f83897a741fcebffb
trung1007 <94527334+trung1007@users.noreply.github.com>
2023-11-23 15:46:11 +0700

    Edit UI

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -82,69 +82,151 @@ const Login = () => {
   };
 
   return (
-    <div className={style.container}>
-      <div className={style.logo}>
-        <img
-          src="Screenshot_2023-11-15_143213-removebg-preview.png"
-          width="200"
-          height="200"
-        ></img>
-        <div className={style.CompanyName}>Magic Post</div>
-      </div>
-      <h2 className={style.title}>Đăng Nhập</h2>
-      <form className={style.form} onSubmit={handleLogin} ref={form}>
-        <div className={style.group}>
-          <label htmlFor="account">Tên tài khoản</label>
-          <input
-            id="account"
+    // <div className={style.container}>
+    //   <div className={style.logo}>
+    //     <img
+    //       src="Screenshot_2023-11-15_143213-removebg-preview.png"
+    //       width="200"
+    //       height="200"
+    //     ></img>
+    //     <div className={style.CompanyName}>Magic Post</div>
+    //   </div>
+    //   <h2 className={style.title}>Đăng Nhập</h2>
+    //   <Form className={style.form} ref={form} onSubmit={handleLogin} >
+    //     <div className={style.group}>
+    //       <label htmlFor="account">Tên tài khoản</label>
+    //       <input
+    //         id="account"
+    //         type="text"
+    //         name="username"
+    //         value={username}
+    //         onChange={onChangeUsername}
+    //         validations={[required]}
+    //       />
+    //     </div>
+    //     <div className={style.group}>
+    //       <label htmlFor="password">Mật Khẩu</label>
+    //       <input
+    //         id="password"
+    //         type="password"
+    //         name="password"
+    //         value={password}
+    //         onChange={onChangePassword}
+    //         validations={[required]}
+    //       />
+    //     </div>
+    //     <div className={style.group}>
+    //       <button ref={checkBtn} disabled={loading}>
+    //         {loading ? (
+    //           <>
+    //             Signing you in... <Spinner size="sm" ml={2} />
+    //           </>
+    //         ) : (
+    //           "Đăng nhập"
+    //         )}
+    //       </button>
+    //       {message && (
+    //         <Flex justifyContent="center" alignItems="center" mt={2}>
+    //           <Text color="teal" fontSize="md">
+    //             {message}
+    //           </Text>
+    //         </Flex>
+    //       )}
+
+    //       {/* <CheckButton style={{ display: "none" }} ref={checkBtn} /> */}
+    //     </div>
+    //   </Form>
+    //   <div className={style.redirect}>
+    //     <p>Bạn chưa có tài khoản ?</p>
+    //     <Link to="/register">Tạo Tài khoản</Link>
+    //   </div>
+    //   <div className={style.license}>
+    //     Khi đăng nhập, tôi đồng ý với các Điều khoản sử dụng và Chính sách bảo
+    //     mật của MagicPost.
+    //   </div>
+    // </div>
+    <Flex
+      direction="column"
+      align="center"
+      justify="center"
+      className={style.container}
+    >
+      {/* Logo */}
+      <img
+        src="Screenshot_2023-11-15_143213-removebg-preview.png"
+        width="200"
+        height="200"
+      ></img>
+      <Text className={style.CompanyName} mb={4}>
+        MagicPost
+      </Text>
+
+      <Heading className={style.title} mb={4}>
+        Login
+      </Heading>
+
+      <Form onSubmit={handleLogin} ref={form} className={style.form}>
+        <FormControl mb={4} className={style.group}>
+          <FormLabel>Username</FormLabel>
+          <Input
             type="text"
+            placeholder="Enter your username"
             name="username"
             value={username}
             onChange={onChangeUsername}
             validations={[required]}
           />
-        </div>
-        <div className={style.group}>
-          <label htmlFor="password">Mật Khẩu</label>
-          <input
-            id="password"
+        </FormControl>
+
+        <FormControl mb={4} className={style.group}>
+          <FormLabel>Password</FormLabel>
+          <Input
             type="password"
+            placeholder="Enter your password"
             name="password"
             value={password}
             onChange={onChangePassword}
             validations={[required]}
           />
-        </div>
-        <div className={style.group}>
-          <button ref={checkBtn} disabled={loading}> 
-            {loading ? (
-              <>
-                Signing you in... <Spinner size="sm" ml={2} />
-              </>
-            ) : (
-              "Đăng nhập"
-            )}
-          </button>
-          {message && (
-            <Flex justifyContent="center" alignItems="center" mt={2}>
-              <Text color="teal" fontSize="md">
-                {message}
-              </Text>
-            </Flex>
-          )}
+        </FormControl>
 
-          {/* <CheckButton style={{ display: "none" }} ref={checkBtn} /> */}
-        </div>
-      </form>
-      <div className={style.redirect}>
-        <p>Bạn chưa có tài khoản ?</p>
-        <Link to="/register">Tạo Tài khoản</Link>
-      </div>
+        <Button
+          colorScheme="teal"
+          type="submit"
+          width="full"
+          disabled={loading}
+        >
+          {loading ? (
+            <>
+              Signing you in... <Spinner size="sm" ml={2} />
+            </>
+          ) : (
+            "Sign In"
+          )}
+        </Button>
+
+        {message && (
+          <Flex justifyContent="center" alignItems="center" mt={2}>
+            <Text color="teal" fontSize="md">
+              {message}
+            </Text>
+          </Flex>
+        )}
+
+        <CheckButton style={{ display: "none" }} ref={checkBtn} />
+      </Form>
+
+      <Box mt={4}>
+        Don't have an account?{" "}
+        <ChakraLink color="teal" href="/register">
+          Register here.
+        </ChakraLink>
+      </Box>
       <div className={style.license}>
         Khi đăng nhập, tôi đồng ý với các Điều khoản sử dụng và Chính sách bảo
         mật của MagicPost.
       </div>
-    </div>
+    </Flex>
   );
 };
 
